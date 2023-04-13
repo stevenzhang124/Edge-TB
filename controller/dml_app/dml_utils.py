46a8b0084dba288cc942a2c6b135f649364e5ad0
stevenzhang124 <1241922639@qq.com>
2023-04-13 21:14:32 +0100

    LeNet test

--- controller/dml_app/dml_utils.py
+++ controller/dml_app/dml_utils.py
@@ -286,11 +286,11 @@ def send_client_weights (weights, path, node_list, connect, clients_layers, forw
 			continue
 		if node in connect:
 			addr = 'http://' + connect [node] + '/' + path
-			data = {'client_layers': str(client_layers)}
+			data = {'client_layers': str(clients_layers)}
 			res = requests.post(addr, json=data, files={'weights': open(client_weight_file, 'rb')})
 		elif forward:
 			addr = 'http://' + connect [node] + '/' + path
-			data = {'client_layers': str(client_layers)}
+			data = {'client_layers': str(clients_layers)}
 			res = requests.post(addr, json=json.dumps(data), files={'weights': open(client_weight_file, 'rb')})
 		else:
 			Exception ('has not connect to ' + node)

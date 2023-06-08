a7c717065f5a7df1ee12a74dff4ca9281369b2aa
stevenzhang124 <1241922639@qq.com>
2023-06-08 20:04:46 +0100

    profile LeNet

--- controller/dml_app/nns/LeNet_mnist_profile.py
+++ controller/dml_app/nns/LeNet_mnist_profile.py
@@ -12,7 +12,7 @@ import time
 # 定义超参数
 batch_size = 128        # 批的大小
 learning_rate = 1e-2    # 学习率
-num_epoches = 5        # 遍历训练集的次数
+num_epoches = 1        # 遍历训练集的次数
 
 # 数据类型转换，转换成numpy类型
 #def to_np(x):
@@ -75,48 +75,59 @@ class LeNet(nn.Module):
         return x
 
 
+def train(model, use_gpu, index, layer_name, criterion, optimizer):
+    # Training loop
+    for epoch in range(num_epoches):
+        print('epoch {}'.format(epoch + 1))      # .format为输出格式，formet括号里的即为左边花括号的输出
+        print('*' * 10)
+        running_loss = 0.0
 
-# Instantiate the model and optimizer
-model = LeNet()
-use_gpu = False
-if use_gpu:
-    model = model.cuda()
+        for i, batch in enumerate(train_loader, 1):
+            inputs, labels = batch
+            if use_gpu:
+                img = img.cuda()
+                label = label.cuda()
+            
+            optimizer.zero_grad()
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+            print("For batch {}".format(i))
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
 
-# "Freeze" the first two layers
-for name, param in model.named_parameters():
-    if 'conv1' in name or 'conv2' in name:
-        param.requires_grad = False
+            start_time = time.time()
+            loss.backward()
+            end_time = time.time()
 
-# Training loop
-for epoch in range(num_epoches):
-    print('epoch {}'.format(epoch + 1))      # .format为输出格式，formet括号里的即为左边花括号的输出
-    print('*' * 10)
-    running_loss = 0.0
+            print("Backward time for layers {}: {}".format(' '.join(layer_name[index+1:-1]), end_time - start_time))
 
-    for i, batch in enumerate(train_loader, 1):
-        inputs, labels = batch
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print('Finish {} epoch, Loss: {:.6f}'.format(
+            epoch + 1, running_loss / (len(train_dataset))))
+
+if __name__ == '__main__':
+    layer_name = ['conv1', 'conv2', 'fc1', 'fc2', 'fc3']
+    for i in range(len(layer_name)-1):
+        freeze_layer = layer_name[0:i+1]
+        # Instantiate the model and optimizer
+        model = LeNet()
+        use_gpu = False
         if use_gpu:
-            img = img.cuda()
-            label = label.cuda()
-        
-        optimizer.zero_grad()
+            model = model.cuda()
+            
+        for name, param in model.named_parameters():
+            for layer in freeze_layer:
+                if layer in name:
+                    param.requires_grad = False
 
-        print("Forward time in batch {}".format(i))
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
+        # start profile
 
-        start_time = time.time()
-        loss.backward()
-        end_time = time.time()
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-        print("Backward time for layers fc1, fc2, fc3 in batch {}: {} seconds".format(i, end_time - start_time))
+        train(model, use_gpu, i, layer_name, criterion, optimizer)
 
-        optimizer.step()
 
-        running_loss += loss.item()
 
-    print('Finish {} epoch, Loss: {:.6f}'.format(
-        epoch + 1, running_loss / (len(train_dataset))))

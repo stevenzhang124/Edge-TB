16772f812f3594d7a8238f27af3d0d1a88f16af8
stevenzhang124 <1241922639@qq.com>
2023-06-09 16:48:57 +0100

    profile VGG-16

--- controller/dml_app/nns/VGG_16_profile.py
+++ controller/dml_app/nns/VGG_16_profile.py
@@ -27,12 +27,12 @@ transform = transforms.Compose(
 	 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 # Load training data
-trainset = torchvision.datasets.CIFAR10(root='../../dataset/', train=True, download=True, transform=transform)
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+trainset = datasets.CIFAR10(root='../../dataset/', train=True, download=True, transform=transform)
+train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
 
 # Load testing data
-testset = torchvision.datasets.CIFAR10(root='../../dataset/', train=False, download=True, transform=transform)
-test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+testset = datasets.CIFAR10(root='../../dataset/', train=False, download=True, transform=transform)
+test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 
 
 

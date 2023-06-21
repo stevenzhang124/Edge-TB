8cd24ea6ba9a6630eb96b1069c17d3379df9d962
stevenzhang124 <1241922639@qq.com>
2023-06-21 13:58:10 +0100

    Profile DNNs

--- controller/dml_app/nns/ResNet101_profile.py
+++ controller/dml_app/nns/ResNet101_profile.py
@@ -44,82 +44,82 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 # Define the ResNet Block
 class Block(nn.Module):
-    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
-        super(Block, self).__init__()
-        self.expansion = 4
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0)
-        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
-        self.relu = nn.ReLU()
-        self.identity_downsample = identity_downsample
+	def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
+		super(Block, self).__init__()
+		self.expansion = 4
+		self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+		self.bn1 = nn.BatchNorm2d(out_channels)
+		self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+		self.bn2 = nn.BatchNorm2d(out_channels)
+		self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0)
+		self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+		self.relu = nn.ReLU()
+		self.identity_downsample = identity_downsample
 
-    def forward(self, x):
-        identity = x
+	def forward(self, x):
+		identity = x
 
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu(x)
-        x = self.conv3(x)
-        x = self.bn3(x)
+		x = self.conv1(x)
+		x = self.bn1(x)
+		x = self.relu(x)
+		x = self.conv2(x)
+		x = self.bn2(x)
+		x = self.relu(x)
+		x = self.conv3(x)
+		x = self.bn3(x)
 
-        if self.identity_downsample is not None:
-            identity = self.identity_downsample(identity)
+		if self.identity_downsample is not None:
+			identity = self.identity_downsample(identity)
 
-        x += identity
-        x = self.relu(x)
-        return x
+		x += identity
+		x = self.relu(x)
+		return x
 
 # Define the ResNet Layer
 def make_layer(block, num_residual_blocks, in_channels, out_channels, stride):
-    identity_downsample = None
-    layers = []
+	identity_downsample = None
+	layers = []
 
-    # Either if we half the input space for ex, 56x56 -> 28x28 (stride=2), or channels change, we need to adapt the Identity (skip connection) so it will be able to be added
-    # to the layer that's ahead
-    if stride != 1 or in_channels != out_channels * 4:
-        identity_downsample = nn.Sequential(nn.Conv2d(in_channels, out_channels * 4, kernel_size=1, stride=stride),
-                                             nn.BatchNorm2d(out_channels * 4))
+	# Either if we half the input space for ex, 56x56 -> 28x28 (stride=2), or channels change, we need to adapt the Identity (skip connection) so it will be able to be added
+	# to the layer that's ahead
+	if stride != 1 or in_channels != out_channels * 4:
+		identity_downsample = nn.Sequential(nn.Conv2d(in_channels, out_channels * 4, kernel_size=1, stride=stride),
+											 nn.BatchNorm2d(out_channels * 4))
 
-    layers.append(block(in_channels, out_channels, identity_downsample, stride))
+	layers.append(block(in_channels, out_channels, identity_downsample, stride))
 
-    # The expansion size is always 4 for ResNet 50,101,152
-    in_channels = out_channels * 4
+	# The expansion size is always 4 for ResNet 50,101,152
+	in_channels = out_channels * 4
 
-    for i in range(num_residual_blocks - 1):
-        layers.append(block(in_channels, out_channels))  # 256 will be mapped to 64, then 64*4 (256 again)...
+	for i in range(num_residual_blocks - 1):
+		layers.append(block(in_channels, out_channels))  # 256 will be mapped to 64, then 64*4 (256 again)...
 
-    return nn.Sequential(*layers)
+	return nn.Sequential(*layers)
 
 # Define the ResNet101
 class ResNet(nn.Module):
-    def __init__(self, block, layers, image_channels, num_classes):
-        super(ResNet, self).__init__()
-        self.in_channels = 64
-        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+	def __init__(self, block, layers, image_channels, num_classes):
+		super(ResNet, self).__init__()
+		self.in_channels = 64
+		self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
+		self.bn1 = nn.BatchNorm2d(64)
+		self.relu = nn.ReLU()
+		self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        # Essentially the entire ResNet architecture are in these 4 lines below
-        self.layer1 = make_layer(block, layers[0], self.in_channels, out_channels=64, stride=1)
-        self.in_channels = 64 * 4 # Update in_channels to prepare for next layer
-        self.layer2 = make_layer(block, layers[1], self.in_channels, out_channels=128, stride=2)
-        self.in_channels = 128 * 4 # Update in_channels to prepare for next layer
-        self.layer3 = make_layer(block, layers[2], self.in_channels, out_channels=256, stride=2)
-        self.in_channels = 256 * 4 # Update in_channels to prepare for next layer
-        self.layer4 = make_layer(block, layers[3], self.in_channels, out_channels=512, stride=2)
+		# Essentially the entire ResNet architecture are in these 4 lines below
+		self.layer1 = make_layer(block, layers[0], self.in_channels, out_channels=64, stride=1)
+		self.in_channels = 64 * 4 # Update in_channels to prepare for next layer
+		self.layer2 = make_layer(block, layers[1], self.in_channels, out_channels=128, stride=2)
+		self.in_channels = 128 * 4 # Update in_channels to prepare for next layer
+		self.layer3 = make_layer(block, layers[2], self.in_channels, out_channels=256, stride=2)
+		self.in_channels = 256 * 4 # Update in_channels to prepare for next layer
+		self.layer4 = make_layer(block, layers[3], self.in_channels, out_channels=512, stride=2)
 
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * 4, num_classes)
+		self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+		self.fc = nn.Linear(512 * 4, num_classes)
 
-    def forward(self, x):
-        start_time = time.time()
+	def forward(self, x):
+		start_time = time.time()
 		x = self.conv1(x)
 		end_time = time.time()
 		print("forward time Conv1: ", end_time-start_time)
@@ -157,22 +157,22 @@ class ResNet(nn.Module):
 		print("forward time layer4: ", end_time-start_time)
 		print("Output size layer4: ", x.element_size() * x.nelement())
 
-        start_time = time.time()
-        x = self.avgpool(x)
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc(x)
-        end_time = time.time()
+		start_time = time.time()
+		x = self.avgpool(x)
+		x = x.reshape(x.shape[0], -1)
+		x = self.fc(x)
+		end_time = time.time()
 		print("forward time fc: ", end_time-start_time)
 		print("Output size fc: ", x.element_size() * x.nelement())
 
-        return x
+		return x
 
 def ResNet101(img_channel=3, num_classes=10):
-    return ResNet101(Block, [3, 4, 23, 3], img_channel, num_classes)
+	return ResNet101(Block, [3, 4, 23, 3], img_channel, num_classes)
 
 
 def ResNet101(img_channel=3, num_classes=10):
-    return ResNet(Block, [3, 4, 23, 3], img_channel, num_classes)
+	return ResNet(Block, [3, 4, 23, 3], img_channel, num_classes)
 
 
 def layer_sizes(model):
